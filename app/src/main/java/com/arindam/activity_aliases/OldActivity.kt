8ab15b3951bb926ed6604482ddfe332173a0aeaf
Arindam Karmakar <arindamxd@gmail.com>
2019-08-07 12:10:08 +0530

    normal and dynamic shortcut implemented

--- app/src/main/java/com/arindam/activity_aliases/OldActivity.kt
+++ app/src/main/java/com/arindam/activity_aliases/OldActivity.kt
@@ -1,7 +1,14 @@
 package com.arindam.activity_aliases
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_old.*
 
 /**
  * Created by Arindam Karmakar on 7/8/19.
@@ -12,5 +19,24 @@ class OldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_old)
+
+        create.setOnClickListener { createDynamicShortcut(this@OldActivity) }
+    }
+
+    /**
+     * An example of creating a dynamic shortcut and associating
+     * it with your app appears in the following code snippet
+     */
+    private fun createDynamicShortcut(context: Context) {
+        val shortcutManager = getSystemService(ShortcutManager::class.java)
+
+        val shortcut = ShortcutInfo.Builder(context, "id1")
+            .setShortLabel("Website")
+            .setLongLabel("Open the website")
+            .setIcon(Icon.createWithResource(context, R.drawable.icon_website))
+            .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("https://arindamxd.github.io/")))
+            .build()
+
+        shortcutManager?.dynamicShortcuts = listOf(shortcut)
     }
 }
